bdee4bfee1ed8bf9f0831ee759f88a1138178b60
parshurampatil197 <parshurampatil197@gmail.com>
2022-09-10 23:53:28 +0530

    category and subcategory fileds made a mandatory fields.

--- CRUD_api/serializers.py
+++ CRUD_api/serializers.py
@@ -3,6 +3,8 @@ from .models import Items
 from django.db.models import fields
 
 class ItemSerializer(serializers.ModelSerializer):
+    category = serializers.CharField(required=True)
+    subcategory = serializers.CharField(required=True)
     class Meta:
         model = Items
         fields = ('category', 'subcategory', 'name', 'amount')
