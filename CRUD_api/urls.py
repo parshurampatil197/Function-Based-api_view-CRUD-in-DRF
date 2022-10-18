96bc95ee88f6387c89600297d1574f04556be6e2
parshurampatil197 <parshurampatil197@gmail.com>
2022-10-18 14:52:12 +0530

    db table name changed to Items

--- CRUD_api/urls.py
+++ CRUD_api/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('v1/', include('CRUD_api.urls'))
+    path('create/', views.add_items, name='add-items'),
+    path('all/', views.view_items, name='view-items'),
+    path('update/<int:pk>', views.update_items, name='update-items'),
+    path('delete/<int:pk>', views.delete_items, name='delete-items')
 ]
